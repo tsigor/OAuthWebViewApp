7806a4f1de1ef26a74abf87500948a0a07c2c612
savelgmx <savel@gmx.com>
2020-07-16 22:06:52 +0700

    links to nessesary pages

--- app/src/main/java/com/example/oauthwebviewapp/MainActivity.java
+++ app/src/main/java/com/example/oauthwebviewapp/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.oauthwebviewapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+https://futurestud.io/tutorials/oauth-2-on-android-with-retrofit
+
+        https://habr.com/ru/post/314028/
+        https://habr.com/ru/post/429058/
+*/
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
